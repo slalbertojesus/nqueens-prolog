c3357c1b03f9ae54f3e026824d848523edda20e3
Alberto Sanchez <slalbertojesus@gmail.com>
2021-06-01 09:23:43 -0500

    Se cambio directorio de estilos

--- servidor.pl
+++ servidor.pl
@@ -3,7 +3,6 @@
 :- use_module(library(http/http_files)).
 :- use_module(library(http/html_write)).
 :- use_module(library(http/http_unix_daemon)).
-:- use_module(library(http/html_head)).	
 
 
 %Locaciones para archivos
@@ -11,16 +10,16 @@
 :- dynamic   http:location/3.
 
 % Obtener archivos para estilos y validaciones. 
-:- http_handler(styles('index.css'),  http_reply_file('index.css', []), []).
 http:location(styles, root(styles), []).
+:- http_handler(styles(.), http_reply_from_files('./styles', []), [prefix]).
 
 % Manejador de urls.
 :- http_handler(root(.), index, []).
 
 % Manejador de solicitudes 
  index(_Request) :-
-    \html_requires(styles('index.css')),
     reply_html_page([title('N Reinas'),
+    link([rel('stylesheet'), href('/styles/index.css') ]),
     h1(class(tituloPrincipal),'Problema N reinas'),
     div([label([for('numeroMatriz')], 'Entero de matriz deseada:'),
     input([class(inputNumeroMatriz),type('number'), id('numeroMatriz'), 
